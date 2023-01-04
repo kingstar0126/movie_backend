d2c57fd87a2cbc2c68a4429f37be2de0aa0b6a0f
kingstar0126 <kingstar0126@outlook.com>
2023-01-04 14:36:18 -0500

    added GET /movies

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors")
+//error handlers 
+const errorHandler = require("./errors/errorHandler");
+const pathNotFound = require("./errors/notFound")
+//require routers files 
+const moviesRouter = require("./movies/movies.router")
 
+//cors and express setup
+app.use(cors())
+app.use(express.json())
+//routes for routers
+app.use("/movies",moviesRouter)
 
-
+app.use(pathNotFound)
+app.use(errorHandler)
 
 
 module.exports = app;
